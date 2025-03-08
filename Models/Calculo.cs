09927fcfee3f537ab3654beeee68f88c1fe7bff1
Fred Duarte <fduartej@gmail.com>
2025-03-08 17:17:43 -0500

    Ejemplo Calculadora

---
+++ Models/Calculo.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apptienda.Models
+{
+    public class Calculo
+    {
+        string? Operacion  { get; set; }
+        double Operador1   { get; set; }
+        double Operador2   { get; set; }
+
+        public double Calcular()
+        {
+            double resultado = 0;
+            switch (Operacion)
+            {
+                case "suma":
+                    resultado = Operador1 + Operador2;
+                    break;
+                case "resta":
+                    resultado = Operador1 - Operador2;
+                    break;
+                case "multiplicar":
+                    resultado = Operador1 * Operador2;
+                    break;
+                case "dividir":
+                    if(Operador2 == 0)
+                    {
+                        throw new DivideByZeroException();
+                    }
+                    resultado = Operador1 / Operador2;
+                    break;
+            }
+            return resultado;
+        }
+    }
+}
