9414545026a0918711eb7ea09bed02f8024e811c
Fred Duarte <fduartej@gmail.com>
2025-03-14 21:36:09 -0500

    cambios en contacto

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace apptienda.Models
 {
     public class Contacto
     {
+        [Required(ErrorMessage = "Nombres son obligatorios.")]
         public string? Nombres { get; set; }
+        [NotNull]
         public string? Email { get; set; }
+        [NotNull]
         public string? Mensaje { get; set; }
     }
 }
